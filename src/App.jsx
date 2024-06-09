f064d93211b85cce1cef2aaabe7e4e9cbbe0903c
Emilio <franceschini.emilio@gmail.com>
2024-06-09 19:59:54 -0300

    add threejs, threejstypes, change css and canvas main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { createSignal } from 'solid-js'
+import { createSignal, onMount } from 'solid-js'
 import './App.css'
 
+let mainCanvas;
+
+function drawInitialGrid(gl) {
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it.",
+    );
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
+
+  onMount(() => drawInitialGrid(mainCanvas.getContext('webgl')))
 
   return (
-    <main>
-      <canvas id ="main_canvas"></canvas>
-    </main>
+    <canvas ref={mainCanvas} id="main_canvas"></canvas>
   )
 }
 
