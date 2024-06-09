0a6850bbb1c1ab30242df3e21044e94022fd1583
Emilio <franceschini.emilio@gmail.com>
2024-06-09 20:08:23 -0300

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 import { createSignal, onMount } from 'solid-js'
 import './App.css'
+import * as THREE from 'three';
 
-let mainCanvas;
 
-function drawInitialGrid(gl) {
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it.",
-    );
-    return;
+function drawInitialGrid() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(512, 512);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate() {
+    renderer.render(scene, camera);
   }
-
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  renderer.setAnimationLoop(animate);
+  document.getElementById('root').appendChild(renderer.domElement);
 }
 
 function App() {
   const [count, setCount] = createSignal(0);
 
-  onMount(() => drawInitialGrid(mainCanvas.getContext('webgl')))
+  onMount(() => drawInitialGrid())
 
-  return (
-    <canvas ref={mainCanvas} id="main_canvas"></canvas>
+  return (<></>
   )
 }
 
